2e46890294d13c0cc74695525721954df67a729c
CoreOS Admin <lars.soonvald@gmail.com>
2018-04-11 11:10:15 +0000

    git clone part moved to run.sh because of volumes

--- drupal-dev/run.sh
+++ drupal-dev/run.sh
@@ -4,5 +4,11 @@
 #  drush --root=/app/drupal -y si config-installer --db-url='pgsql://drupaluser:drupalpassword@db/drupaldb' --account-name=admin --account-pass=tarkmees
 #fi
 
+git clone  https://github.com/lsoonvald/HTM.git /app
+#composer --working-dir=/app require drupal/config_installer
+#composer --working-dir=/app update
+
+chown apache.apache -R /app/web/site/defalt/files
+
 echo "[i] Starting daemon..."
 httpd -D FOREGROUND
